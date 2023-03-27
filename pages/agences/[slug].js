0d271dc8c2d774116f3914f53f4b64bdc57237b1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-03-27 15:06:33 +0200

    Create [slug].js

---
+++ pages/agences/[slug].js
@@ -0,0 +1,63 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { getAgencys } from "../../data/agencys";
+import AgenceLayout from "../../components/Layout/AgenceLayout";
+
+export default function Agence() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const agency = getAgencys.find((agency) => agency.slug === slug);
+
+  if (!agency) {
+    return <div>Agence non trouvée</div>;
+  }
+
+  return (
+    <>
+      <AgenceLayout title={agency.title} />
+    </>
+  );
+}
+
+// pages/agences/[slug].js
+// import { useRouter } from "next/router";
+// import { getAgencys } from "../../data/agencys";
+// import AgenceLayout from "../../components/Layout/AgenceLayout";
+
+// export default function Agence({ agency }) {
+//   const router = useRouter();
+
+//   if (router.isFallback) {
+//     return <div>Chargement...</div>;
+//   }
+
+//   if (!agency) {
+//     return <div>Agence non trouvée</div>;
+//   }
+
+//   return (
+//     <>
+//       <AgenceLayout title={agency.title} />
+//     </>
+//   );
+// }
+
+// export async function getStaticPaths() {
+//   const paths = getAgencys.map((agency) => ({
+//     params: { slug: agency.slug },
+//   }));
+
+//   return { paths, fallback: true };
+// }
+
+// export async function getStaticProps({ params }) {
+//   const agency = getAgencys.find((agency) => agency.slug === params.slug);
+
+//   return {
+//     props: {
+//       agency,
+//     },
+//     revalidate: 1,
+//   };
+// }
