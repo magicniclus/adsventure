9c5772bdc439be8f6f521df7c4520cac15c7194a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-03-30 09:00:35 +0200

    Update index.js

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -10,10 +10,11 @@ const index = () => {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl text-center">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              From the blog
+              Actualité Adventure
             </h2>
             <p className="mt-2 text-lg leading-8 text-gray-600">
-              Learn how to grow your business with our expert advice.
+              Découvrez notre actualité ainsi que quelques informations
+              importantes sur Adventure et Google Ads.
             </p>
           </div>
           <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-y-20 gap-x-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
@@ -23,11 +24,13 @@ const index = () => {
                 className="flex flex-col items-start justify-between"
               >
                 <div className="relative w-full">
-                  <img
-                    src={post.imageUrl}
-                    alt=""
-                    className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-                  />
+                  <Link href={`/blog/${post.slug}`}>
+                    <img
+                      src={post.imageUrl}
+                      alt=""
+                      className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                    />
+                  </Link>
                   <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
                 </div>
                 <div className="max-w-xl">
@@ -54,11 +57,11 @@ const index = () => {
                     </p>
                   </div>
                   <div className="relative mt-8 flex items-center gap-x-4">
-                    <img
-                      src={post.author.imageUrl}
-                      alt=""
-                      className="h-10 w-10 rounded-full bg-gray-100"
-                    />
+                    <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-gray-500 mr-3">
+                      <span className="text-xs font-medium leading-none text-white">
+                        NC
+                      </span>
+                    </span>
                     <div className="text-sm leading-6">
                       <p className="font-semibold text-gray-900">
                         <a href={post.author.href}>
