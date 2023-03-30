801aaa2e7629bbb3177d27ea69e6f5020e9d782e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-03-30 09:00:37 +0200

    Update getContent.js

--- utils/getContent.js
+++ utils/getContent.js
@@ -1,3 +1,5 @@
+import { CheckBadgeIcon } from "@heroicons/react/20/solid";
+
 export const getContent = (content) => {
   switch (content.tag) {
     case "h2":
@@ -20,6 +22,22 @@ export const getContent = (content) => {
       );
     case "p":
       return <p className="mt-6 text-xl leading-8">{content.content}</p>;
+    case "ul":
+      return (
+        <ul className="mt-6 text-xl leading-8 list-disc list-inside">
+          {content.content.map((listItem) => (
+            <div className="flex">
+              <CheckBadgeIcon
+                className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                aria-hidden="true"
+              />
+              <li className="ml-5" key={listItem.tag + listItem.content}>
+                {listItem.content}
+              </li>
+            </div>
+          ))}
+        </ul>
+      );
     default:
       return null;
   }
