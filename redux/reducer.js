7585e16cb082a34390d8168f2a7f8257e15b5b3b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-03-30 15:21:57 +0200

    Update reducer.js

--- redux/reducer.js
+++ redux/reducer.js
@@ -1,7 +1,32 @@
-const initState = {};
+const initState = {
+  errorModal: {
+    show: false,
+    message: "",
+  },
+  validModal: {
+    show: false,
+    message: "",
+  },
+};
 
 const reducer = (state = initState, action) => {
   switch (action.type) {
+    case "UPDATE_ERROR_MODAL":
+      return {
+        ...state,
+        errorModal: {
+          show: action.payload.show,
+          message: action.payload.message,
+        },
+      };
+    case "UPDATE_VALID_MODAL":
+      return {
+        ...state,
+        validModal: {
+          show: action.payload.show,
+          message: action.payload.message,
+        },
+      };
     default:
       return state;
   }
