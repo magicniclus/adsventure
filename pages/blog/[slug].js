6e10b7a3329867205821c85a4bbe46262d362929
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-03-30 17:19:45 +0200

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,11 +2,33 @@ import { useRouter } from "next/router";
 import posts from "../../data/posts";
 import BlogLayout from "../../components/Layout/BlogLayout";
 
-export default function BlogPost() {
-  const router = useRouter();
-  const { slug } = router.query;
+// export default function BlogPost() {
+//   const router = useRouter();
+//   const { slug } = router.query;
 
-  const post = posts.find((post) => post.slug === slug);
+//   const post = posts.find((post) => post.slug === slug);
+
+//   if (!post) {
+//     return (
+//       <div className="h-full w-full flex justify-center items-center">
+//         Article non trouvé
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <>
+//       <BlogLayout datas={post} />
+//     </>
+//   );
+// }
+
+export default function BlogPost({ post }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Chargement...</div>;
+  }
 
   if (!post) {
     return <div>Article non trouvé</div>;
@@ -14,49 +36,26 @@ export default function BlogPost() {
 
   return (
     <>
-      <BlogLayout datas={post} />
+      <BlogLayout title={post.title} datas={post} />
     </>
   );
 }
 
-// // pages/blog/[slug].js
-// import { useRouter } from "next/router";
-// import posts from "../../data/posts";
-// import BlogLayout from "../../components/Layout/BlogLayout";
+export async function getStaticPaths() {
+  const paths = posts.map((post) => ({
+    params: { slug: post.slug },
+  }));
 
-// export default function BlogPost({ post }) {
-//   const router = useRouter();
+  return { paths, fallback: true };
+}
 
-//   if (router.isFallback) {
-//     return <div>Chargement...</div>;
-//   }
+export async function getStaticProps({ params }) {
+  const post = posts.find((post) => post.slug === params.slug);
 
-//   if (!post) {
-//     return <div>Article non trouvé</div>;
-//   }
-
-//   return (
-//     <>
-//       <BlogLayout title={post.title} />
-//     </>
-//   );
-// }
-
-// export async function getStaticPaths() {
-//   const paths = posts.map((post) => ({
-//     params: { slug: post.slug },
-//   }));
-
-//   return { paths, fallback: true };
-// }
-
-// export async function getStaticProps({ params }) {
-//   const post = posts.find((post) => post.slug === params.slug);
-
-//   return {
-//     props: {
-//       post,
-//     },
-//     revalidate: 1,
-//   };
-// }
+  return {
+    props: {
+      post,
+    },
+    revalidate: 1,
+  };
+}
