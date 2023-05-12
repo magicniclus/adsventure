f6ca3c1e5363ca24c17e16ab9e1ce0a29abbcd5b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-12 17:22:26 +0200

    Create HeroWithForm.js

---
+++ components/Organisms/Hero/HeroWithForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import InscriptionEntreprise from "../Forms/InscriptionEntreprise";
+
+const HeroWithForm = () => {
+  return (
+    <div>
+      <InscriptionEntreprise />
+    </div>
+  );
+};
+
+export default HeroWithForm;
