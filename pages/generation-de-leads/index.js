b007af4870ac1cc50c33984d1e69518f567dc5d7
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-12 17:22:31 +0200

    Update index.js

--- pages/generation-de-leads/index.js
+++ pages/generation-de-leads/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import LandingPage from "../../components/Layout/LandingPage";
+import HeroWithForm from "../../components/Organisms/Hero/HeroWithForm";
 
 const index = () => {
-  return <div></div>;
+  return (
+    <LandingPage
+      description="Contactez Adsventure, l'agence SEA spécialisée dans l'acquisition de leads. Obtenez une consultation dès maintenant !"
+      title="Contactez-nous pour une stratégie d'acquisition de leads"
+      canonical="contact"
+    >
+      <HeroWithForm />
+    </LandingPage>
+  );
 };
 
 export default index;
