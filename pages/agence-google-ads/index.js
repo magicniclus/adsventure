0632938764ece6ce2f20a688a5304dfade84008d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-05 15:11:25 +0200

    Update index.js

--- pages/agence-google-ads/index.js
+++ pages/agence-google-ads/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import LandingPage from "../../components/Layout/LandingPage";
 import HeroLandingPage from "../../components/Organisms/Hero/HeroLandingPage";
+import GoogleAssets from "../../components/Organisms/section/GoogleAssets";
+import Trust from "../../components/Organisms/section/Trust";
 
 const index = () => {
   return (
     <LandingPage certificate={true}>
       <HeroLandingPage />
+      <GoogleAssets />
+      <Trust />
     </LandingPage>
   );
 };
