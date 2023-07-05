ecf21912da02a182b5b77d90085e7a6e827d5204
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-05 15:26:44 +0200

    Update index.js

--- pages/agence-google-ads/index.js
+++ pages/agence-google-ads/index.js
@@ -3,6 +3,7 @@ import LandingPage from "../../components/Layout/LandingPage";
 import HeroLandingPage from "../../components/Organisms/Hero/HeroLandingPage";
 import GoogleAssets from "../../components/Organisms/section/GoogleAssets";
 import Trust from "../../components/Organisms/section/Trust";
+import AllGoogleService from "../../components/Organisms/section/AllGoogleService";
 
 const index = () => {
   return (
@@ -10,6 +11,7 @@ const index = () => {
       <HeroLandingPage />
       <GoogleAssets />
       <Trust />
+      <AllGoogleService />
     </LandingPage>
   );
 };
