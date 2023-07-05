47655232fcaae360ca2062f638398f411a764ad6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-05 13:46:19 +0200

    Create HeroLandingPage.js

---
+++ components/Organisms/Hero/HeroLandingPage.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+const HeroLandingPage = () => {
+  return (
+    <div className="relative isolate overflow-hidden bg-white">
+      <svg
+        className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+        aria-hidden="true"
+      >
+        <defs>
+          <pattern
+            id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
+            width={200}
+            height={200}
+            x="50%"
+            y={-1}
+            patternUnits="userSpaceOnUse"
+          >
+            <path d="M.5 200V.5H200" fill="none" />
+          </pattern>
+        </defs>
+        <rect
+          width="100%"
+          height="100%"
+          strokeWidth={0}
+          fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)"
+        />
+      </svg>
+      <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-28">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
+          <div className="mt-24 sm:mt-32 lg:mt-16">
+            <h2 className="font-semibold text-indigo-600 text-lg">
+              Agence Google ads
+            </h2>
+          </div>
+          <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Votre ROI pourrait augmenter de manière spectaculaire grâce à{" "}
+            <span className="text-indigo-600">Google Ads</span>
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Augmentez vos ventes en ligne, trouvez des prospects qualifiés ou
+            améliorez la notoriété de votre marque.
+          </p>
+          <div className="mt-10 flex items-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Que le spectacle commence <span aria-hidden="true">→</span>
+            </a>
+          </div>
+        </div>
+        <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+          <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none lg:flex lg:items-center">
+            <div className="-m-2 rounded-xl p-2 lg:-m-4 lg:rounded-2xl lg:p-6">
+              <img
+                src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
+                alt="App screenshot"
+                width={2200}
+                height={1200}
+                className="w-[62rem] rounded-md shadow-2xl ring-1 ring-gray-900/10 lg:rotate-12 lg:mt-16"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroLandingPage;
