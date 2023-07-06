5b2e0ea4efe187909798bd076c84601f45de8224
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-06 12:30:15 +0200

    Update Trust.js

--- components/Organisms/section/Trust.js
+++ components/Organisms/section/Trust.js
@@ -5,42 +5,40 @@ const Trust = () => {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:max-w-none">
-          <h2 className="text-lg font-semibold leading-8 text-gray-900">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Ils nous ont fait confience
           </h2>
-          <div className="mx-auto mt-10 grid grid-cols-4 items-start gap-x-8 gap-y-10 sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:grid-cols-5">
+          <div className="mx-auto mt-14 grid grid-cols-4 items-center gap-x-8 gap-y-10 sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:grid-cols-5">
             <img
               className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
-              src="https://tailwindui.com/img/logos/transistor-logo-gray-900.svg"
-              alt="Transistor"
+              src="/images/logo/terabois.png"
+              alt="terabois"
+              width={158}
+              height={48}
+            />
+            <img
+              className="col-span-2 max-h-12 max-w-[150px] object-left lg:col-span-1"
+              src="/images/logo/portailimmo.png"
+              alt="avenueimmo"
+            />
+            <img
+              className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
+              src="/images/logo/maprimerenov.png"
+              alt="maprimerenov"
               width={158}
               height={48}
             />
             <img
               className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
-              src="https://tailwindui.com/img/logos/reform-logo-gray-900.svg"
-              alt="Reform"
+              src="/images/logo/conciergeriecastera.png"
+              alt="conciergerie-castera"
               width={158}
               height={48}
             />
             <img
               className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
-              src="https://tailwindui.com/img/logos/tuple-logo-gray-900.svg"
-              alt="Tuple"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
-              src="https://tailwindui.com/img/logos/savvycal-logo-gray-900.svg"
-              alt="SavvyCal"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
-              src="https://tailwindui.com/img/logos/statamic-logo-gray-900.svg"
-              alt="Statamic"
+              src="/images/logo/portailhabitat.png"
+              alt="portail-habitat"
               width={158}
               height={48}
             />
