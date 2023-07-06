631397b7fe6ed035e6bf17272b5eccfc9d945ebb
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-06 12:30:13 +0200

    Update TestimonialGads.js

--- components/Organisms/section/TestimonialGads.js
+++ components/Organisms/section/TestimonialGads.js
@@ -1,15 +1,29 @@
 import React from "react";
 const people = [
   {
-    name: "Leslie Alexander",
-    role: "Co-Founder / CEO",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-    bio: "Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.",
-    twitterUrl: "#",
-    linkedinUrl: "#",
+    name: "avenue-immo.com",
+    tags: "Plateforme immobilière",
+    imageUrl: "/images/logo/portailimmo.png",
+    bio: "Avenue-immo.com est un portail immobilier qui permet aux professonels de générer des demandes d'estimation qualifiers et de vendre leurs biens.",
+    CPA: "22€",
+    conversion: "+ 150%",
   },
-  // More people...
+  {
+    name: "MaPrimeRenov-info.org",
+    tags: "Amélioration de l'habitat",
+    imageUrl: "/images/logo/maprimerenov.png",
+    bio: "MaPrimeRenov-info.fr est un site d'information sur la rénovation énergétique en France. Il permet aux particuliers de trouver des informations sur les aides financières disponibles pour la rénovation énergétique de leur logement.",
+    CPA: "8,5€",
+    conversion: "+ 300%",
+  },
+  {
+    name: "Terabois.fr",
+    tags: "Entreprise de construction et de renovation",
+    imageUrl: "/images/logo/terabois.png",
+    bio: "Terabois est une entreprise de construction et de rénovation de maisons individuelles.",
+    CPA: "16€",
+    conversion: "+ 100%",
+  },
 ];
 const TestimonialGads = () => {
   return (
@@ -17,12 +31,20 @@ const TestimonialGads = () => {
       <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-5">
         <div className="max-w-2xl xl:col-span-2">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            About the team
+            L'investissement marketing le plus rentable pour vous
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            We’re a dynamic group of individuals who are passionate about what
-            we do and dedicated to delivering the best results for our clients.
+            Adsventure est le partenaire de beaucoup d'entreprises qui ont fait
+            le choix de la croissance.
           </p>
+          <div className="mt-10 flex items-center gap-x-6 w-full ">
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Prendre rendez-vous <span aria-hidden="true">→</span>
+            </a>
+          </div>
         </div>
         <ul
           role="list"
@@ -34,7 +56,7 @@ const TestimonialGads = () => {
               className="flex flex-col gap-10 pt-12 sm:flex-row"
             >
               <img
-                className="aspect-[4/5] w-52 flex-none rounded-2xl object-cover"
+                className="aspect-[4/5] w-52 flex-none rounded-2xl object-contain"
                 src={person.imageUrl}
                 alt=""
               />
@@ -43,47 +65,23 @@ const TestimonialGads = () => {
                   {person.name}
                 </h3>
                 <p className="text-base leading-7 text-gray-600">
-                  {person.role}
+                  {person.tags}
                 </p>
                 <p className="mt-6 text-base leading-7 text-gray-600">
                   {person.bio}
                 </p>
                 <ul role="list" className="mt-6 flex gap-x-6">
-                  <li>
-                    <a
-                      href={person.twitterUrl}
-                      className="text-gray-400 hover:text-gray-500"
-                    >
-                      <span className="sr-only">Twitter</span>
-                      <svg
-                        className="h-5 w-5"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                      </svg>
-                    </a>
+                  <li className="text-gray-600">
+                    Coût par aquisition moyen <br />
+                    <span className="font-bold text-green-600 text-xl">
+                      {person.CPA}
+                    </span>
                   </li>
-                  <li>
-                    <a
-                      href={person.linkedinUrl}
-                      className="text-gray-400 hover:text-gray-500"
-                    >
-                      <span className="sr-only">LinkedIn</span>
-                      <svg
-                        className="h-5 w-5"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </a>
+                  <li className="text-gray-600">
+                    Taux de conversion moyen <br />
+                    <span className="font-bold text-green-600 text-xl">
+                      {person.conversion}
+                    </span>
                   </li>
                 </ul>
               </div>
