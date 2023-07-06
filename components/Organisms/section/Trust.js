2036897a381d5af2ccf3dbde2bbe9087dd66be4c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-06 14:38:55 +0200

    Update Trust.js

--- components/Organisms/section/Trust.js
+++ components/Organisms/section/Trust.js
@@ -8,25 +8,24 @@ const Trust = () => {
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Ils nous ont fait confience
           </h2>
-          <div className="mx-auto mt-14 grid grid-cols-4 items-center gap-x-8 gap-y-10 sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:grid-cols-5">
+          <div className="mx-auto mt-14 flex justify-between w-full bg-slate-100">
+            <div className="flex flex-col justify-center items-center">
+              <img
+                className="max-h-12 w-full object-cover object-left"
+                src="/images/logo/terabois.png"
+                alt="terabois"
+              />
+              <h3>Terabois</h3>
+            </div>
             <img
-              className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
-              src="/images/logo/terabois.png"
-              alt="terabois"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 max-h-12 max-w-[150px] object-left lg:col-span-1"
-              src="/images/logo/portailimmo.png"
+              className="max-h-12 w-full object-contain object-left"
+              src="/images/logo/avenueimmo.png"
               alt="avenueimmo"
             />
             <img
               className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
               src="/images/logo/maprimerenov.png"
               alt="maprimerenov"
-              width={158}
-              height={48}
             />
             <img
               className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
