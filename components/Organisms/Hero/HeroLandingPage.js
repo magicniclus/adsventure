47e9527951460a15dd9ef797267bf776dacc7ee6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-06 15:43:47 +0200

    Update HeroLandingPage.js

--- components/Organisms/Hero/HeroLandingPage.js
+++ components/Organisms/Hero/HeroLandingPage.js
@@ -46,7 +46,7 @@ const HeroLandingPage = () => {
               href="#"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Que le spectacle commence <span aria-hidden="true">→</span>
+              Let's go <span aria-hidden="true">→</span>
             </a>
           </div>
         </div>
